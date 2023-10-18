51d00f4cb67154d31cedfa3feacc6675511530ed
probeshnath <probeshn@gmail.com>
2023-10-18 18:49:05 +0600

    React router setup done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import { Link,NavLink } from 'react-router-dom'
 
 const Navbar = () => {
     const links = <>
-        <li><NavLink to="/" className={({ isActive, isPending }) => isActive ? "border-b-2 border-orange-600"  : ""} > Home </NavLink>  </li>
-        <li><NavLink to="/about" className={({ isActive, isPending }) => isActive ? "border-b-2 border-orange-600"  : ""} > About </NavLink>  </li>
-        <li><NavLink to="/cart" className={({ isActive, isPending }) => isActive ? "border-b-2 border-orange-600"  : ""} > My cart </NavLink>  </li>
-        <li><NavLink to="/addproduct" className={({ isActive, isPending }) => isActive ? "border-b-2 border-orange-600"  : ""} > Add Product </NavLink>  </li>
+        <li><NavLink to="/" className={({ isActive, isPending }) => isActive ? "border-b-2 font-bold text-white  border-orange-600"  : ""} > Home </NavLink>  </li>
+        <li><NavLink to="/about" className={({ isActive, isPending }) => isActive ? "border-b-2 font-bold text-white border-orange-600"  : ""} > About </NavLink>  </li>
+        <li><NavLink to="/cart" className={({ isActive, isPending }) => isActive ? "border-b-2 font-bold text-white border-orange-600"  : ""} > My cart </NavLink>  </li>
+        <li><NavLink to="/addproduct" className={({ isActive }) => isActive ? "border-b-2 font-bold text-white border-orange-600"  : ""} > Add Product </NavLink>  </li>
 
     </>
-    // : isPending ? "pending"
+  
     return (
         <div className='bg-black bg-opacity-90 text-white'>
             <div className=' max-w-6xl mx-auto flex justify-between items-center'>
@@ -24,7 +24,7 @@ const Navbar = () => {
                     </ul>
                 </div>
                 {/* user || login */}
-                <div >
+                <div className=' mr-2 text-gray-300' >
                     <Link to="/login"><button className='border-2 border-orange-600 py-1 px-4 rounded-2xl'>Login</button></Link>
                 </div>
             </div>
--- src/route/Route.jsx
+++ src/route/Route.jsx
@@ -2,6 +2,11 @@ import { createBrowserRouter } from "react-router-dom";
 import ErrorPage from "../pages/ErrorPage";
 import MainRoute from "./MainRoute";
 import Home from "../pages/Home";
+import About from "../pages/About";
+import Cart from "../pages/Cart";
+import AddProduct from "../pages/AddProduct";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
   
   const router = createBrowserRouter([
     {
@@ -12,7 +17,27 @@ import Home from "../pages/Home";
         {
             path:"/",
             element: <Home />
-        }
+        },
+        {
+            path:"/about",
+            element: <About />
+        },
+        {
+            path:"/cart",
+            element: <Cart />
+        },
+        {
+            path:"/addproduct",
+            element: <AddProduct />
+        },
+        {
+            path:"/login",
+            element: <Login />
+        },
+        {
+            path:"/register",
+            element: <Register />
+        },
       ]
 
     },
