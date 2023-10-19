6709a28bba434689952ec4405e0f9e6b00ad3fa8
probeshnath <probeshn@gmail.com>
2023-10-20 02:35:09 +0600

    product add in cart to MongoDB

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link, useLoaderData } from 'react-router-dom'
+import {AuthContext} from '../Provider/Provider'
 
 const ProductDetails = () => {
     const product = useLoaderData();
     // console.log("product details",product)
+    const {user} =useContext(AuthContext)
+
+    const addtoCart = (product) =>{
+      const cartData = {
+        email: user.email,
+        product
+      }
+      fetch("http://localhost:5000/cart",{
+        method:"POST",
+        headers:{
+          'content-type':"application/json"
+        },
+        body: JSON.stringify(cartData)
+      })
+      .then(res => res.json())
+      .then((data)=> {
+        console.log(data)
+      })
+    }
+
   return (
     <div className='max-w-6xl mx-auto pt-[100px]'>
          <figure><img src={product.productImg} alt="Shoes" /></figure>
@@ -16,7 +37,7 @@ const ProductDetails = () => {
                     <p>{product.rating}</p>
                     <div>
                         <Link to={`/update/${product._id}`} ><button className='btn'>Update</button></Link>
-                        <Link to={`/products/${product._id}`}><button className='btn'>Buy Product</button></Link>
+                        <button onClick={()=> addtoCart(product)} className='btn'>Add to Cart</button>
                     </div>
                     
                 </div>
